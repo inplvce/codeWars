8030efa9507b56a64dbb3a3e0b5cd109182b4493
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-08 14:33:36 +0300

    n

--- add.js
+++ add.js
@@ -1,10 +1,10 @@
-let a;
-a = 3;
-console.log(a);
+let aa;
+aa = 3;
+console.log(aa);
 
 // Определите переменную aс помощью varи инициализируйте ее значением 9.
 
-var b = 9;
+var bb = 9;
 
 /* 
 Создайте две новые строковые переменные: 
@@ -14,3 +14,23 @@ myFirstNameи myLastNameи присвойте им значения вашего
 const myFirstName = "my";
 const myLastName = " name";
 console.log(myFirstName + myLastName);
+
+/*
+Инициализируйте три переменные a, b, и cс 5, 10, и "I am a"соответственно так, 
+чтобы их не было undefined.
+*/
+
+let a = 5;
+let b = 10;
+let c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+// Измените существующие объявления и назначения, чтобы в их именах использовался camelCase.
+
+studlyCapVaR = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;

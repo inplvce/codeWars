2d800094d3ed91c9b3d7b7f042c495c0e2420570
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-08 14:31:02 +0300

    n

--- add.js
+++ add.js
@@ -5,3 +5,12 @@ console.log(a);
 // Определите переменную aс помощью varи инициализируйте ее значением 9.
 
 var b = 9;
+
+/* 
+Создайте две новые строковые переменные: 
+myFirstNameи myLastNameи присвойте им значения вашего имени и фамилии соответственно.
+*/
+
+const myFirstName = "my";
+const myLastName = " name";
+console.log(myFirstName + myLastName);
